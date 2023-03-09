5aa0986375df0770b619ad7a14c50bb7639153f5
Apisit Sirichoke (อภิสิทธิ์ ศิริโชค) <Apisit.Sirichoke@quantaqmb.com>
2023-03-09 18:48:28 +0700

    First commit

---
+++ GetPickDataInfor/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace GetPickDataInfor
+{
+    partial class frmGetPickDataInfor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpStop = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnExcute = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ptbLoad = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.bgwLoad = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbLoad)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpStart.Location = new System.Drawing.Point(148, 22);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(165, 31);
+            this.dtpStart.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel3);
+            this.groupBox1.Controls.Add(this.panel2);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(534, 530);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "FORM GET PICKDATAINFO BY DATE";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(46, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "StartDate";
+            // 
+            // dtpStop
+            // 
+            this.dtpStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpStop.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpStop.Location = new System.Drawing.Point(148, 67);
+            this.dtpStop.Name = "dtpStop";
+            this.dtpStop.Size = new System.Drawing.Size(165, 31);
+            this.dtpStop.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "StopDate";
+            // 
+            // btnExcute
+            // 
+            this.btnExcute.BackColor = System.Drawing.Color.Lime;
+            this.btnExcute.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExcute.ForeColor = System.Drawing.Color.Green;
+            this.btnExcute.Location = new System.Drawing.Point(319, 22);
+            this.btnExcute.Name = "btnExcute";
+            this.btnExcute.Size = new System.Drawing.Size(157, 76);
+            this.btnExcute.TabIndex = 4;
+            this.btnExcute.Text = "Excute";
+            this.btnExcute.UseVisualStyleBackColor = false;
+            this.btnExcute.Click += new System.EventHandler(this.btnExcute_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnExcute);
+            this.panel1.Controls.Add(this.dtpStart);
+            this.panel1.Controls.Add(this.dtpStop);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(3, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(528, 123);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.ptbLoad);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(3, 150);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(528, 316);
+            this.panel2.TabIndex = 6;
+            // 
+            // ptbLoad
+            // 
+            this.ptbLoad.Image = global::GetPickDataInfor.Properties.Resources.Loading;
+            this.ptbLoad.Location = new System.Drawing.Point(62, 6);
+            this.ptbLoad.Name = "ptbLoad";
+            this.ptbLoad.Size = new System.Drawing.Size(394, 304);
+            this.ptbLoad.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbLoad.TabIndex = 0;
+            this.ptbLoad.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.lbResult);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(3, 466);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(528, 61);
+            this.panel3.TabIndex = 7;
+            // 
+            // lbResult
+            // 
+            this.lbResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResult.Location = new System.Drawing.Point(0, 0);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(528, 61);
+            this.lbResult.TabIndex = 0;
+            this.lbResult.Text = "test000000000000000000000000000000000000000000000000000000000";
+            // 
+            // bgwLoad
+            // 
+            this.bgwLoad.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwLoad_DoWork);
+            this.bgwLoad.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgwLoad_RunWorkerCompleted);
+            // 
+            // frmGetPickDataInfor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(534, 530);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmGetPickDataInfor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GetPickDataInfor [02/01/2023]";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmGetPickDataInfor_FormClosing);
+            this.Load += new System.EventHandler(this.frmGetPickDataInfor_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbLoad)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox ptbLoad;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExcute;
+        private System.Windows.Forms.DateTimePicker dtpStop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lbResult;
+        private System.ComponentModel.BackgroundWorker bgwLoad;
+    }
+}
+
---
+++ GetPickDataInfor/Form1.cs
@@ -0,0 +1,128 @@
+﻿using Connect.BLL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetPickDataInfor
+{
+    public partial class frmGetPickDataInfor : Form
+    {
+        ConnectDB oCon = new ConnectDB();
+        int flag = 0;
+        public frmGetPickDataInfor()
+        {
+            InitializeComponent();
+        }
+
+        private void frmGetPickDataInfor_Load(object sender, EventArgs e)
+        {
+            lbResult.Text = "";
+            SetDefault();
+        }
+
+        public void SetDefault()
+        {            
+            dtpStart.Value = DateTime.Now;
+            dtpStop.Value = DateTime.Now;
+            dtpStart.Enabled = true;
+            dtpStop.Enabled = true;
+
+            btnExcute.Enabled = true;
+
+            ptbLoad.Visible = false;            
+        }
+
+        private void bgwLoad_DoWork(object sender, DoWorkEventArgs e)
+        {
+            SqlCommand sql = new SqlCommand();
+
+            try
+            {
+                string tmpdtpStart = dtpStart.Text.Trim();
+                string tmpdtpStop = dtpStop.Text.Trim();
+
+                sql.CommandText = "EXEC GetPickDataInforByDate '" + tmpdtpStart.Replace("/", "") + "', '" + tmpdtpStop.Replace("/", "") + "'";
+                sql.CommandTimeout = 180;
+                oCon.ExecuteCommand(sql);
+
+                flag = 1;
+            }
+            catch (Exception ex)
+            {
+                playNG();
+                lbResult.ForeColor = Color.Red;
+                lbResult.Text = "Result: " + ex.ToString().Trim();
+                flag = 0;
+            }
+        }
+
+        private void bgwLoad_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            SetDefault();
+            if (flag == 1)
+            {
+                playOK();
+                lbResult.ForeColor = Color.Green;
+                lbResult.Text = "Result: Email sent. Please check your email again.";
+            }
+        }
+
+        private void btnExcute_Click(object sender, EventArgs e)
+        {           
+            if (!bgwLoad.IsBusy)
+            {
+                lbResult.Text = "";
+                setDisable();
+                bgwLoad.RunWorkerAsync();
+            }
+        }
+
+        public void setDisable()
+        {
+            ptbLoad.Visible = true;
+
+            btnExcute.Enabled = false;
+
+            dtpStart.Enabled = false;
+            dtpStop.Enabled = false;
+        }
+
+        public void playOK()
+        {
+            string exePath = Application.StartupPath + "\\OK.wav";
+            SoundPlayer simpleSound = new SoundPlayer(exePath);
+            simpleSound.Play();
+            return;
+        }
+
+        public void playNG()
+        {
+            string exePath = Application.StartupPath + "\\OO.wav";
+            SoundPlayer simpleSound = new SoundPlayer(exePath);
+            simpleSound.Play();
+            return;
+        }
+
+        private void frmGetPickDataInfor_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Do you want to close the program ?", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.DoEvents();
+            }
+            else
+            {
+                e.Cancel = true;
+                SetDefault();
+                lbResult.Text = "";
+            }
+        }
+    }
+}
